b77431db606cac58020c8880c49911ffaa79b7a5
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-06-09 20:56:22 +0530

    First part Completed

---
+++ src/Navbar.jsx
@@ -0,0 +1,26 @@
+let Navbar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">GetKart</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Cart</a>
+            </li>
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Theme</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
