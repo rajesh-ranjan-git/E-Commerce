3ad338464ebc24aef898f4d993dfb080b615c099
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-28 20:10:34 +0530

    AuthWrapper

---
+++ src/AuthWrapper.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { usersAPI } from "./Constants";
+import { Navigate } from "react-router";
+import ShimmerUI from "./ShimmerUI";
+
+const AuthWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const auth = async () => {
+    try {
+      const response = await axios.get(usersAPI, { withCredentials: true });
+
+      setIsAuthenticated(response.data.result);
+    } catch (err) {
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    auth();
+  }, []);
+
+  if (isLoading === true) {
+    return <ShimmerUI />;
+  } else {
+    return isAuthenticated === true ? children : <Navigate to="/login" />;
+  }
+};
+
+export default AuthWrapper;
